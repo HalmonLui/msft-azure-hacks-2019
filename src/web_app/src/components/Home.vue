96d25ad886d25a5e8bbaf976615f1ab4fb384512
Lui <luih@wit.edu>
2019-08-24 23:50:41 -0400

    Created Article component

--- src/web_app/src/components/Home.vue
+++ src/web_app/src/components/Home.vue
@@ -1,12 +1,28 @@
 <template>
   <div class="home">
+    <hr id="home_hr" />
+    <div class="home_stocks_container">
+      <p>Subcomponent C Goes Here</p>
+    </div>
+    <hr id="home_hr" />
     <h1>{{ msg }}</h1>
+    <h2>INSERT TITLE HERE BIG ARTICLE BOI GOES HERE</h2>
+    <div class="home_articles_container">
+      <Article></Article>
+      <Article></Article>
+      <Article></Article>
+      <Article></Article>
+    </div>
   </div>
 </template>
 
 <script>
+import Article from "./subcomponents/Article";
 export default {
   name: "Home",
+  components: {
+    Article: Article
+  },
   data() {
     return {
       msg: "Welcome to Finance Boi"
---
+++ src/web_app/src/components/subcomponents/Article.vue
@@ -0,0 +1,17 @@
+<template>
+  <a class="article">
+    <div class="article_image_container">
+      <img
+        src="https://i.ytimg.com/vi/if-2M3K1tqk/maxresdefault.jpg"
+        alt="Finance Boi Logo"
+        class="article_image"
+      />
+    </div>
+    <div class="article_text_container">
+      <h2 class="article_title">Stonks are on the rise</h2>
+      <p class="article_info">
+        There are tons of info on stonks. For more checkout r/wallstreetbets
+      </p>
+    </div>
+  </a>
+</template>
