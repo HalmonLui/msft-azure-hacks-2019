69467df8c36644932cdbb34e2d565dc341cb6f00
Sonam Ghosh <sonamghosh@gmail.com>
2019-08-24 23:57:38 -0700

    Reformated the functionality into 2 functions for grabbing closing price data

--- src/pipeline/utils.py
+++ src/pipeline/utils.py
@@ -3,24 +3,28 @@ import numpy as np
 import requests
 import datetime
 import arrow
-
+import os
 
 # Grab data from Yahoo Finance
 def grab_data(ticker, date_range, date_interval):
     # @TODO: consider a ticker column
     res = requests.get(
         'https://query1.finance.yahoo.com/v8/finance/chart/{ticker}?range={date_range}&interval={date_interval}'.format(**locals()))
-    #data = res.json()
-    #body = data['chart']['result'][0]
     data = res.json()['chart']['result'][0]
-    dt = pd.Series(map(lambda x: arrow.get(x).datetime.replace(tzinfo=None), data['timestamp']), name='date')
-    df = pd.DataFrame(data['indicators']['quote'][0], index=dt)
-    df.index = df.index.normalize()
-    #df = df.reset_index()
+    return data
 
+
+def transform_data(data_json):
+    dt = pd.Series(map(lambda x: arrow.get(x).datetime.replace(
+        tzinfo=None), data_json['timestamp']), name='date')
+    df = pd.DataFrame(data_json['indicators']['quote'][0], index=dt)
+    df.index = df.index.normalize()
     df = df[['close']]
-    print(df.head(10))
+
+    return df
 
 
 if __name__ == "__main__":
-    grab_data('aapl', date_range='1y', date_interval='1d')
+    a = grab_data('aapl', date_range='1y', date_interval='1d')
+    b = transform_data(a)
+    print(b.head(10))
