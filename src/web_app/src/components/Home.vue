51ccc748460dcfbeb2f0c4bee42c0918f1ea41d5
Lui <luih@wit.edu>
2019-08-25 20:36:51 -0400

    2 column articles, added watchlist dynamically

--- src/web_app/src/components/Home.vue
+++ src/web_app/src/components/Home.vue
@@ -25,13 +25,24 @@
         </p>
       </div>
     </a>
-    <div class="home_articles_container">
-      <Article
-        v-for="article in articles"
-        v-bind:title="article.title"
-        v-bind:text="article.text"
-        v-bind:article_image="article.article_image"
-      ></Article>
+    <div class="home_bottom_section">
+      <div class="home_articles_container">
+        <Article
+          v-for="article in articles"
+          v-bind:title="article.title"
+          v-bind:text="article.text"
+          v-bind:article_image="article.article_image"
+        ></Article>
+      </div>
+      <div class="home_watchlist_container">
+        <h2 class="home_watchlist_title">Watch List</h2>
+        <WatchItem
+          v-for="watchitem in watchitems"
+          v-bind:symbol="watchitem.symbol"
+          v-bind:price="watchitem.price"
+          v-bind:change="watchitem.change"
+        ></WatchItem>
+      </div>
     </div>
     <h1>{{ msg }}</h1>
   </div>
@@ -40,11 +51,13 @@
 <script>
 import Article from "./subcomponents/Article";
 import Stock from "./subcomponents/Stock";
+import WatchItem from "./subcomponents/WatchItem";
 export default {
   name: "Home",
   components: {
     Article: Article,
-    Stock: Stock
+    Stock: Stock,
+    WatchItem: WatchItem
   },
   data() {
     return {
@@ -58,7 +71,36 @@ export default {
         { symbol: "SNAM", price: "1337", change: "+69%" },
         { symbol: "BRHE", price: "1234", change: "+44%" }
       ],
+      watchitems: [
+        { symbol: "GOOG", price: "1111", change: "+5%" },
+        { symbol: "MSFT", price: "420", change: "+6%" },
+        { symbol: "AMZN", price: "5555", change: "+10%" },
+        { symbol: "TSLA", price: "5555", change: "+10%" },
+        { symbol: "KEVT", price: "4455", change: "+40%" },
+        { symbol: "SNAM", price: "1337", change: "+69%" },
+        { symbol: "BRHE", price: "1234", change: "+44%" }
+      ],
       articles: [
+        {
+          title: "Stonks are on the rise",
+          text:
+            "There are tons of info on stonks. For more checkout r/wallstreetbets"
+        },
+        {
+          title: "Stonks are on the rise",
+          text:
+            "There are tons of info on stonks. For more checkout r/wallstreetbets"
+        },
+        {
+          title: "Stonks are on the rise",
+          text:
+            "There are tons of info on stonks. For more checkout r/wallstreetbets"
+        },
+        {
+          title: "Stonks are on the rise",
+          text:
+            "There are tons of info on stonks. For more checkout r/wallstreetbets"
+        },
         {
           title: "Stonks are on the rise",
           text:
---
+++ src/web_app/src/components/subcomponents/WatchItem.vue
@@ -0,0 +1,25 @@
+<template>
+  <a class="watchitem" href="https://www.google.com">
+    <div class="watchitem_image_container">
+      <img
+        src="https://int-financial.com/wp-content/uploads/2018/08/Stock-Market-Adults-Online-copy-17072018090131-1000x0.jpg"
+        alt="Finance Boi Logo"
+        class="watchitem_image"
+      />
+    </div>
+    <div class="watchitem_text_container">
+      <h3 class="watchitem_title">{{ symbol }}</h3>
+      <p class="watchitem_info">${{ price }}</p>
+    </div>
+  </a>
+</template>
+
+<script>
+export default {
+  name: "WatchItem", //this is the name of the component
+  props: ["symbol", "price", "change"],
+  data() {
+    return {};
+  }
+};
+</script>
