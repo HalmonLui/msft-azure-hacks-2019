e57ce991ce0067b9b9c2750721dbc1d9300cb0fb
Brian He <brian.he17@gmail.com>
2019-09-09 00:14:33 -0400

    user,pass,front-end

--- src/web_app/src/components/Register.vue
+++ src/web_app/src/components/Register.vue
@@ -2,17 +2,6 @@
   <div>
     <h1>Join Finance Boi</h1>
     <p>Already have an account? <a href="http://localhost:11964/?#/login">Login</a> </p>
-  <!--<p>
-    <ul>
-      <li>Whats ur name</li>
-      <li>Please provide your email</li>
-      <li>Mother's maiden name</li>
-      <li>Age</li>
-      <li>Name of your first pet</li>
-      <li>SSID</li>
-      <li>Isssssd</li>
-    </ul>
-  </p>-->
   <form>
     First name:<br>
     <input type="text" name="firstname"><br><br>
