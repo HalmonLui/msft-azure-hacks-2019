5041903621b7bcb253483c34f2b980d3ab0ff285
Lui <luih@wit.edu>
2019-09-10 16:02:25 -0400

    Getting ready for build and merge

--- src/web_app/src/main.js
+++ src/web_app/src/main.js
@@ -3,7 +3,7 @@
 import Vue from "vue";
 import App from "./App";
 import router from "./router";
-import store from "@/store";
+import store from "./store";
 import firebase from "firebase";
 
 Vue.config.productionTip = false;
