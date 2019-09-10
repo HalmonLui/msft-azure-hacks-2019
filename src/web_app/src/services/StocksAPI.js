8fb1f66db8cbfaedd155754a7995c406f1a65517
Lui <luih@wit.edu>
2019-09-10 16:56:07 -0400

    prepping for merge

--- src/web_app/src/services/StocksAPI.js
+++
@@ -1,9 +0,0 @@
-import API from "@/services/API";
-export default {
-  getStocks() {
-    return API().get("stocks");
-  },
-  getStock(ticker) {
-    return API().get("stock", { params: { ticker: ticker } });
-  }
-};
